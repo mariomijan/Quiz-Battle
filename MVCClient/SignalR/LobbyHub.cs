bfbbcca53ede10cdcd0cf342a5e326b930b670c2
mariomijan <mijanmario@gmail.com>
2018-01-25 21:22:37 +0100

    Add files via upload

---
+++ MVCClient/SignalR/LobbyHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using MVCClient.LobbyReference;
+using MVCClient.Models;
+using MVCClient.UserReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace MVCClient.SignalR
+{
+    public class LobbyHub : Hub
+    {
+        private UserServiceClient userClient = new UserServiceClient();
+
+        public LobbyHub()
+        {
+
+        }
+
+        public void Join(int lobbyId)
+        {
+            Groups.Add(Context.ConnectionId, lobbyId.ToString());
+
+            var users = userClient.GetUsersInLobby(lobbyId);
+            Clients.Group(lobbyId.ToString()).getUsersInLobby(users);
+        }
+    }
+}
---
+++ MVCClient/SignalR/SignalRScripts.js
@@ -0,0 +1,50 @@
+﻿$(function () {
+    // Declare a proxy to reference the hub. 
+    var lobbyHub = $.connection.lobbyHub;
+    InitializeServerCallableFunctions();
+    //start up the hub
+    $.connection.hub.start().done(function () {
+
+        lobbyHub.server.join("XXl", "testhandle");
+
+        //lobbyHub.server.getLobbies();
+        //$('#sendmessage').click(function () {
+        //    // Call the Send method on the hub. 
+        //    chat.server.join("testLobby");
+        //    // Clear text box and reset focus for next comment. 
+        //    $('#message').val('').focus();
+        //});
+    });
+
+    function InitializeServerCallableFunctions() {
+
+        lobbyHub.client.joinLobby = function (lobbyName, handle) {
+            // Html encode display name and message. 
+            var encodedName = $('<div />').text(lobbyName).html();
+            var encodedMsg = $('<div />').text(lobbyName).html();
+            // Add the message to the page. 
+            $('#discussion').append('<li><strong>' + encodedName
+                + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+        };
+        lobbyHub.client.leaveLobby = function (lobbyName) {
+            // Html encode display name and message. 
+            var encodedName = $('<div />').text(lobbyName).html();
+            var encodedMsg = $('<div />').text(lobbyName).html();
+            // Add the message to the page. 
+            $('#discussion').append('<li><strong>' + encodedName
+                + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+        };
+        lobbyHub.client.logToConsole = function (msg) {
+            console.log(msg, "testLobby");
+        };
+        lobbyHub.client.displayLobbies = function (lobbies) {
+            console.log(lobbies);
+            for (var i = 0; i < lobbies.length; i++) {
+                console.log(lobbies[i]);
+                $('#lobbies-container').append(lobbies[i].Name + "<br/>");
+            }
+        };
+
+
+    }
+});
